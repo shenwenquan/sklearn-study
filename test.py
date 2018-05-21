419cf0af828a3355d38ad3f86c36e1d3730c8ec0
ice <grxshen@hotmail.com>
2018-05-21 23:23:37 +0800

    import 2

--- test.py
+++ test.py
@@ -4,11 +4,16 @@ arr = [1, 1, 2, 5]
 
 print(np.bincount(arr))
 
-
-a = [1,2,3]
-b = [4,5,6]
-print(list(zip(a,b)))
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(list(zip(a, b)))
 
 C_s = np.logspace(-10, 0, 10)
 
 print(C_s)
+
+from sklearn.metrics import confusion_matrix
+
+y_true = [2, 0, 2, 2, 0, 1]
+y_pred = [0, 0, 2, 2, 0, 2]
+print(confusion_matrix(y_true, y_pred))
